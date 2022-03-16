a8cdd55b72f0c605ce93ff1df380476e405ca7a0
Goncharova Margo <goncharovarita@mail.ru>
2022-03-16 21:15:29 +0300

    task 4 complete

--- Mod_9task4/Mod_9task4.js
+++ Mod_9task4/Mod_9task4.js
@@ -1,7 +1,7 @@
 const myButton = document.querySelector('.j-btn');
 const insertData = document.querySelector('.insertData')
 
-function formUrl() {
+function formRequest() {
   let data = Array.from(document.querySelectorAll('.input')).map(input => +input.value);
   
   function isValidNumber (element, index, array){
@@ -13,8 +13,13 @@ function formUrl() {
     const width = arr[0];
     const height = arr[1];
     let url = `https://picsum.photos/${width}/${height}`;
-    console.log(url)
-       return useRequest(url, displayResult)
+    
+   fetch(url)
+  .then((response) => { 
+    insertData.innerHTML = `
+   <div class="card"><img src="${response.url}"></div>`
+     })
+    
   } else {
     insertData.innerHTML = 'Одно из чисел вне диапазона от 100 до 300'
   }; 
@@ -23,30 +28,6 @@ function formUrl() {
   elementArr(data);
 }
 
-// function displayResult(apiData){
-// let cards= '';
-
-// apiData.forEach(item =>{
-//     const cardBlock = `<div class = "card"> <img src= "${item.download_url}" class = "image"/><p>${item.autor}</p> </div>`
-//     cards += cardBlock;
-// });
-// insertData.innerHTML = cards;
-// }
-
-   const useRequest = (url, callback) => {
-   return fetch(url)
-  .then((response) => {
-    console.log('response', response);
-    return response.json();
-     })
-  .then((json) => { return console.log(json);})
-  .catch(() => { console.log('error') }) 
-    
-  }
-      
-
 myButton.addEventListener('click', async() => {
-  formUrl()
-  const requestResult = await useRequest();
-    console.log('end');     
+    const requestResult = await formRequest();  
 })
